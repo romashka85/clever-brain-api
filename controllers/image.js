a208cca0b482d350569f320dc9271b57bcd5ba94
Yulia <duratsia@students.softuni.bg>
2019-06-05 13:54:41 +0300

    adding clarify

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'efdd94c425bd471d9ceefd63cb863702'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data)
+        })   
+        .catch(err => res.status(400).json('unable to work with API'))
+  
+}   
+
 const handleImage = (req, res, db)=>{
     const { id } = req.body;
         db.select('*').from('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db)=>{
     }
 
     module.exports = {
-        handleImage: handleImage
+        handleImage: handleImage,
+        handleApiCall:handleApiCall
     };
